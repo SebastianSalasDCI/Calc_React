2c4dc38daa9a478e8a271886ea4755375b0bb38e
SebastianSalasDCI <55529652+SebastianSalasDCI@users.noreply.github.com>
2021-01-21 14:42:39 +0100

    Update KeyBoard.jsx
    I fixed the button problem

--- src/Components/KeyBoard/KeyBoard.jsx
+++ src/Components/KeyBoard/KeyBoard.jsx
@@ -13,7 +13,7 @@ export class KeyBoard extends Component {
                 <Number name="firstNumber" id="firstNumber" number={this.props.num1} onchangeFunc={this.props.onchangeFunc}/>
                 <Number name="secondNumber" id="secondNumber" number={this.props.num2} onchangeFunc={this.props.onchangeFunc}/> 
 
-                <Button classNprop="btn1" name="Plus" onclickFunc={this.props.onclickFunc}/>
+                <Button classNprop="btn1" name="Pluss" onclickFunc={this.props.onclickFunc}/>
                 <Button classNprop="btn2" name="Minus" onclickFunc={this.props.onclickFunc}/>
                 <Button classNprop="btn3" name="Divide" onclickFunc={this.props.onclickFunc}/>
                 <Button classNprop="btn4" name="Multiply" onclickFunc={this.props.onclickFunc}/>
